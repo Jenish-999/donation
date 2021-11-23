e02961e9511db3a7d220319492eee29982e2bcf8
Siddharth-101 <94886542+Siddharth-101@users.noreply.github.com>
2021-11-23 11:33:37 +0530

    Just updated this para

--- trust.php
+++ trust.php
@@ -74,14 +74,15 @@ body {
                 <h2>Your Trust is our reputation</h2>
                 <div class="col py-1">
                     Where does it come from?
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                    classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin
-                    professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
-                    consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical
-                    literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-                    of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This
-                    book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of
-                    Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                    Contrary to popular belief, Lorem Ipsum is not simply random text.
+                    This is message to all git user that follow this steps along the way
+                    here i would like to convey that nothing we can do.simply random text.
+                    This is message to all git user that follow this steps along the way
+                    here i would like to convey that nothing we can do.simply random text.
+                    This is message to all git user that follow this steps along the way
+                    here i would like to convey that nothing we can do.simply random text.
+                    This is message to all git user that follow this steps along the way
+                    here i would like to convey that nothing we can do.
                 </div>
                
             </div>
